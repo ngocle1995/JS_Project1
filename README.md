09b6cea3ac168a317dcfcd9defb5f61ef050dbcc
Ngoc Le <2005636@laurea.fi->
2021-10-21 20:41:52 +0300

    final

--- README.md
+++ README.md
@@ -1,2 +1,49 @@
 # Project1
 A JavaScript project
+Student name: Ngoc Le
+Student id: 2005636
+Link to GitHub: https://github.com/ngocle1995/JS_Project1.git
+
+App description:
+The app is a simple and the most basic todo list app which has the functions:
+-add task
+-mark completed task
+-delete task
+-show current date
+-Error indicators when input length is less than 2 letters.
+-local Storage: save and clear.
+The layout present a mobile-device layout.
+
+My own evaluation: 3
+Reason: I managed to include all the basic functions and variables and understand the DOM scripting and forms.
+However, i was struggling with fixing the css code for todo, trash-btn and complete-btn, where the complete and trash buttons move according to the length of the text shown in the list.
+I was also planning to add a filter, however, it did not work and it took me 4-5 days in vain trying to fix it. I copied the function here so if it is possible to fix it, I would very appreciate.
+
+function filterTask(e) {
+    const task = list.childNodes;
+    task.forEach(function (todo) {
+            switch (e.target.value) {
+                case "all":
+                    todo.style.display = "flex";
+                    break;
+                case "completed":
+                    if (todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+                case "pending":
+                    if (!todo.classList.contains("completed")) {
+                        todo.style.display = "flex";
+                    } else {
+                        todo.style.display = "none";
+                    }
+                    break;
+            }
+        });
+    
+}
+
+Overall, I do believe that I would be able to add more functions and tailor my todo List more. But unfortunately, i underestimate the work load of 5 other courses I am taking at the same time.
+Therefore, I only have one week to work on this project and i was disappointed at how it turned out in the end.
---
+++ src/js/main.js
@@ -0,0 +1,91 @@
+const clear = document.querySelector(".clear");
+const dateElement = document.getElementById("date");
+const list = document.getElementById("list");
+const todoInput = document.getElementById("input");
+const todoButton = document.querySelector(".todo-button");
+
+let todos = [];
+
+todoButton.addEventListener("click", addTodo);
+list.addEventListener("click",deleteCheck);
+clear.addEventListener("click", function(){
+    localStorage.clear();
+    location.reload();
+});
+
+// Show todays date
+const options = {weekday : "long", month:"short", day:"numeric"};
+const today = new Date();
+
+dateElement.innerHTML = today.toLocaleDateString("en-US", options);
+
+function addTodo (event) {
+    event.preventDefault();
+    document.getElementById("input").style.border = '';
+    document.getElementById("input").style.color = '';
+    let task = todoInput.value;
+    if(task.length > 2){
+        const todoDiv = document.createElement("todo");
+        todoDiv.classList.add("todo");
+        const newTodo = document.createElement('li');
+        newTodo.innerText = todoInput.value;
+        newTodo.classList.add('todo-item');
+        todoDiv.appendChild(newTodo);
+
+        const completedButton = document.createElement('button');
+        completedButton.innerHTML = '<i class="fas fa-check"></i>'
+        completedButton.classList.add("complete-btn");
+        todoDiv.appendChild(completedButton);
+
+        const trashButton = document.createElement('button');
+        trashButton.innerHTML = '<i class="fas fa-trash"></i>'
+        trashButton.classList.add("trash-btn");
+        todoDiv.appendChild(trashButton);
+
+        list.appendChild(todoDiv);
+    } else {
+        document.getElementById("input").style.border = "2px solid red";
+        document.getElementById("input").style.color = "#EF0000";  
+        alert("Input need more then 2 characters");
+    }
+        todos.push(task);
+        saveLocalTodo(todos);
+    todoInput.value= "";
+}
+
+function deleteCheck(e){
+    const item = e.target;
+    if(item.classList[0] === 'trash-btn'){
+    const todo = item.parentElement;
+    todo.remove();
+    }
+
+    if(item.classList[0] === 'complete-btn'){
+        const todo = item.parentElement;
+        todo.classList.toggle("completed");
+    }
+}
+
+
+
+function saveLocalTodo(todos){
+    if(localStorage.getItem("todos") === null){
+        todos = [];
+    } else {
+        todos = JSON.parse(localStorage.getItem("todos"));
+    }
+
+    localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+
+
+
+
+
+
+
+
+
+
+
