db6fd81a73a8f74f0b5638da4612a2bdc3b4091b
Ngoc Le <2005636@laurea.fi->
2021-10-22 13:53:57 +0300

    456

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@ const list = document.getElementById("list");
 const todoInput = document.getElementById("input");
 const todoButton = document.querySelector(".todo-button");
 
+//Identify storage array
 let todos = [];
 
 //Event listeners
@@ -27,18 +28,20 @@ function addTodo (event) {
     document.getElementById("input").style.color = '';
     let task = todoInput.value;
     if(task.length > 2 && task.length < 23){
+        //Create div for tasks
         const todoDiv = document.createElement("todo");
         todoDiv.classList.add("todo");
+        //Categorized input
         const newTodo = document.createElement('li');
         newTodo.innerText = todoInput.value;
         newTodo.classList.add('todo-item');
         todoDiv.appendChild(newTodo);
-
+        //Complete button
         const completedButton = document.createElement('button');
         completedButton.innerHTML = '<i class="fas fa-check"></i>'
         completedButton.classList.add("complete-btn");
         todoDiv.appendChild(completedButton);
-
+        //Trash button
         const trashButton = document.createElement('button');
         trashButton.innerHTML = '<i class="fas fa-trash"></i>'
         trashButton.classList.add("trash-btn");
@@ -62,7 +65,7 @@ function deleteCheck(e){
     const todo = item.parentElement;
     todo.remove();
     }
-
+//Check and categorized the input
     if(item.classList[0] === 'complete-btn'){
         const todo = item.parentElement;
         todo.classList.toggle("completed");
